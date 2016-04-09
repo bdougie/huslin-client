d829f5e484ade3ac02dda0297a2f3b7de171dc1b
Brian Douglas <ilikerobot@gmail.com>
2016-04-09 00:54:38 -0700

    styles updated

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
-      'ember-bootstrap': {
+      'ember-cli-bootstrap-sass': {
         'importBootstrapTheme': true
       }
     });

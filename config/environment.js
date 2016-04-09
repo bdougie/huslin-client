a19e61dfc2afaf0deaa669e12cc57169b0c09ff6
Brian Douglas <ilikerobot@gmail.com>
2016-04-09 00:13:05 -0700

    fixes styles

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'client',
+    modulePrefix: 'hussln-client',
     environment: environment,
     apiUrl: 'http://hussln.herokuapp.com',
     baseURL: '/',
@@ -23,9 +23,9 @@ module.exports = function(environment) {
   if (environment === 'development') {
     ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {

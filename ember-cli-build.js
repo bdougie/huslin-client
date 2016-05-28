7772baf0c5e506482e00fbb113f44578ad6a35c6
Brian Douglas <ilikerobot@gmail.com>
2016-05-28 04:35:16 -0700

    update remove SRI integrity addition

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
+      SRI: {
+        enabled: false,
+      },
+
       'ember-cli-bootstrap-sass': {
         'importBootstrapTheme': true
       }
